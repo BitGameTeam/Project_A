a89e97bc1ad39415f8ead5392d4f7d73817f8a25
jinhee0092 <56423230+jinhee0092@users.noreply.github.com>
2019-12-12 22:48:58 +0900

    a
    a

---
+++ jjh/TowerDefence/TimeWave/Assets/Scripts/RightMouseButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightMouseButton : MonoBehaviour
+{
+    private void Update()
+    {
+        OpenCreateTower();
+    }
+
+    private void OpenCreateTower()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
